eebb94a0047f338e9a639e0e55a4e139387210ba
David Pires <developer@davidpires.pt>
2022-08-27 00:44:24 +0100

    Add logger

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
 use clap::Parser;
 use dinovelc::cli::{Cli, Commands};
 
+#[macro_use]
+extern crate log;
+
 fn main() {
+  env_logger::init();
+
+  info!("Starting dinovelc");
   let args = Cli::parse();
 
   match &args.command {
